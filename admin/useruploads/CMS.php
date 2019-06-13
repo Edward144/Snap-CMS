d5a5d97587bf00907b3770f92dccaaf0abaf083c
Edward144 <edward.hall.144@gmail.com>
2019-06-13 10:30:10 +0100

    Removed test useruploads and added to gitignore

--- admin/useruploads/CMS.php
+++
@@ -1,9 +0,0 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/admin/templates/header.php'); ?>
-    
-    <?php include_once($_SERVER['DOCUMENT_ROOT'] . '/admin/templates/sidebar.php'); ?>
-
-    <div class="content">
-        this is the admin.
-    </div>
-
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/admin/templates/footer.php'); ?>
