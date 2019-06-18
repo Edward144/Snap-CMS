8057ffab49302e8b11c7c2f081b0f59f41f8fdb8
Edward Hall <edward.hall.144@gmail.com>
2019-06-18 15:44:59 +0100

    Functionality to create custom posts added

--- admin/settings/scripts/generalSettings.js
+++ admin/settings/scripts/generalSettings.js
@@ -20,3 +20,43 @@ $("#generalSettings input[type='submit']").click(function() {
         }
     });
 });
+
+//Add Post Type
+$("#customPosts input[name='addRow']").click(function() {
+    $("#customPosts table").append(
+        "<tr>" + 
+            "<td><input type='text' name='postTypeName' placeholder='Name'></td>" + 
+            "<td><input type='button' class='badButton' name='delete' value='Delete'></td>" + 
+        "</tr>"
+    );
+});
+
+//Update Post Types
+$("#customPosts input[type='submit']").click(function() {
+    event.preventDefault();
+    
+    var i = 1;
+    
+    $("#customPosts .message").text("");
+    
+    $("#customPosts table tr:not(.headers)").each(function() {
+        var name = $(this).find("input[name='postTypeName']").val();
+        
+        if(name == "") {
+            $("#customPosts .message").append("Row " + i + " skipped, missing name.<br>");
+        }
+        else {
+            $.ajax({
+                url: "scripts/updateCustomPosts.php",
+                method: "POST",
+                dataType: "json",
+                data: ({name}),
+                success: function(data) {
+                    $("#customPosts .message").append(data);
+                }
+            });
+        }
+        
+        i++;
+    });    
+});
