3c26c8377e72350c5a639075915b4d7cc20d6698
Edward Hall <edward.hall.144@gmail.com>
2019-06-21 10:52:07 +0100

    Added wrap div

--- 404.php
+++ 404.php
@@ -1,9 +1,11 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/templates/header.php'); ?>
     
     <?php include_once($_SERVER['DOCUMENT_ROOT'] . '/templates/sidebar.php'); ?>
-
-    <div class="content">
-        <h1>This page does not exist!</h1>
+    
+    <div class="mainInner">
+        <div class="content">
+            <h1>This page does not exist!</h1>
+        </div>
     </div>
     
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/templates/footer.php'); ?>
