70a4225f8fe4510e7a7b2882a9ba8730ee7c5764
Edward Hall <edward.hall.144@gmail.com>
2019-07-26 13:45:12 +0100

    Update gallery carousel

--- templates/functions.php
+++ templates/functions.php
@@ -1320,6 +1320,29 @@
                             }
                             
                             $postOutput .= '</div>';
+                            
+                            $postOutput .= 
+                                '<script>
+                                    $(document).ready(function(){
+                                        $(".owl-carousel").owlCarousel({
+                                            loop:false,
+                                            margin:0,
+                                            nav:true,
+                                            responsive:{
+                                                0:{
+                                                    items:1
+                                                },
+                                                600:{
+                                                    items:3
+                                                },
+                                                1000:{
+                                                    items:6
+                                                }
+                                            },
+                                            dots: false
+                                        });
+                                    });
+                                </script>';
                         }
                     
                         $sideOptions = '';
