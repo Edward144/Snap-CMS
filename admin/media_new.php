c22fd1e699d405bc77678f16c6104d2c5688147b
Edward Hall <edward.hall.144@gmail.com>
2019-08-14 11:55:51 +0100

    Updated link

--- admin/media_new.php
+++ admin/media_new.php
@@ -5,7 +5,7 @@
     <div class="content">
         <h1><?php adminTItle(); ?></h1>
         
-        <iframe src="//<?php echo $_SERVER['SERVER_NAME']; ?>/admin/tinymce/plugins/moxiemanager" style="width: 100%; height: 100%;"></iframe>
+        <iframe src="/admin/tinymce/plugins/moxiemanager/index" style="width: 100%; height: 100%;"></iframe>
     </div>
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/admin/templates/footer.php'); ?>
