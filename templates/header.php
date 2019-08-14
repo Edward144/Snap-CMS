7e2106eb31505f446c6735da62140ce6d0049081
Edward Hall <edward.hall.144@gmail.com>
2019-08-14 15:00:49 +0100

    Added unique page class

--- templates/header.php
+++ templates/header.php
@@ -89,5 +89,5 @@
         
         <?php include_once($_SERVER['DOCUMENT_ROOT'] . '/templates/navigation.php'); ?>
 
-        <div class="main">
+        <div class="main <?php echo str_replace('/', '_', $_SERVER['REQUEST_URI']); ?>">
             
