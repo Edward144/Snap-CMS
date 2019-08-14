db55f3c2840e6934438bdfaf2ab6d97ad237b265
Edward Hall <edward.hall.144@gmail.com>
2019-08-14 12:03:21 +0100

    Updated browser height

--- admin/media.php
+++ admin/media.php
@@ -2,10 +2,8 @@
     
     <?php include_once($_SERVER['DOCUMENT_ROOT'] . '/admin/templates/sidebar.php'); ?>
 
-    <div class="content">
-        <h1><?php adminTItle(); ?></h1>
-        
-        <iframe src="/admin/tinymce/plugins/moxiemanager/index" style="width: 100%; height: 100%;"></iframe>
+    <div class="content">        
+        <iframe src="/admin/tinymce/plugins/moxiemanager/index" style="width: 100%; height: 98%;"></iframe>
     </div>
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/admin/templates/footer.php'); ?>
