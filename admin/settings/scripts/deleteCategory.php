dfd9440ded3d5b56bd5e753195507961f2efcf9b
Edward Hall <edward.hall.144@gmail.com>
2019-08-30 11:49:59 +0100

    Sub Categories Move Up
    Sub categories are now changed to top level, when their parent category is deleted.

--- admin/settings/scripts/deleteCategory.php
+++ admin/settings/scripts/deleteCategory.php
@@ -5,6 +5,7 @@
     $id = $_POST['id'];
 
     $mysqli->query("DELETE FROM `categories` WHERE id = {$id}");
+    $mysqli->query("UPDATE `categories` SET parent_id = 0, level = 0 WHERE parent_id = {$id}");
 
     if(!$mysqli->error) {
         echo json_encode([1, 'Category deleted successfully.']);
