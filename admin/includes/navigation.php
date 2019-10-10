9f8fb855e8d369723ad990181446c21fe441c6a5
Edward Hall <edward.hall.144@gmail.com>
2019-10-10 15:21:49 +0100

    Changed colour scheme to blue/orange

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -3,7 +3,7 @@
 <nav id="adminNav">
     <ul>
         <li>
-            <a href="<?php echo ROOT_DIR; ?>admin">Dashboard</a>
+            <a href="<?php echo ROOT_DIR; ?>admin" <?php echo ($_SERVER['REQUEST_URI'] == ROOT_DIR . 'admin/' ? 'id="active"' : ''); ?>>Dashboard</a>
         </li>
         
         <li class="hasChildren">
@@ -23,15 +23,15 @@
         </li>
         
         <li>
-            <a href="<?php echo ROOT_DIR; ?>admin/categories">Categories</a>
+            <a href="<?php echo ROOT_DIR; ?>admin/categories" <?php echo ($_SERVER['REQUEST_URI'] == ROOT_DIR . 'admin/categories' ? 'id="active"' : ''); ?>>Categories</a>
         </li>
         
         <li>
-            <a href="<?php echo ROOT_DIR; ?>admin/navigation">Navigation</a>
+            <a href="<?php echo ROOT_DIR; ?>admin/navigation" <?php echo ($_SERVER['REQUEST_URI'] == ROOT_DIR . 'admin/navigation' ? 'id="active"' : ''); ?>>Navigation</a>
         </li>
         
         <li>
-            <a href="<?php echo ROOT_DIR; ?>admin/sliders">Sliders</a>
+            <a href="<?php echo ROOT_DIR; ?>admin/sliders" <?php echo ($_SERVER['REQUEST_URI'] == ROOT_DIR . 'admin/sliders' ? 'id="active"' : ''); ?>>Sliders</a>
         </li>
         
         <li>
@@ -42,9 +42,9 @@
             <a>Settings</a>
             
             <ul class="subMenu">
-                <li><a href="<?php echo ROOT_DIR; ?>admin/user-management">User Management</a></li>
-                <li><a href="<?php echo ROOT_DIR; ?>admin/company-details">Company Details</a></li>
-                <li><a href="<?php echo ROOT_DIR; ?>admin/general-settings">General Settings</a></li>
+                <li><a href="<?php echo ROOT_DIR; ?>admin/user-management" <?php echo ($_SERVER['REQUEST_URI'] == ROOT_DIR . 'admin/user-management' ? 'id="active"' : ''); ?>>User Management</a></li>
+                <li><a href="<?php echo ROOT_DIR; ?>admin/company-details" <?php echo ($_SERVER['REQUEST_URI'] == ROOT_DIR . 'admin/company-details' ? 'id="active"' : ''); ?>>Company Details</a></li>
+                <li><a href="<?php echo ROOT_DIR; ?>admin/general-settings" <?php echo ($_SERVER['REQUEST_URI'] == ROOT_DIR . 'admin/general-settings' ? 'id="active"' : ''); ?>>General Settings</a></li>
             </ul>
         </li>
     </ul>
