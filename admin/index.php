5a110ddccf464b5f806651dd9c7d1cfb8b354d4d
Edward Hall <edward.hall.144@gmail.com>
2019-10-10 14:49:38 +0100

    Completed Dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,34 +1,37 @@
 <?php require_once('includes/header.php'); ?>
 
-<?php $postTypes = $mysqli->query("SELECT name FROM `post_types` ORDER BY name ASC"); ?>
+<?php $postTypes = $mysqli->query("SELECT id, name FROM `post_types` ORDER BY name ASC"); ?>
 <?php if($postTypes->num_rows > 0) : ?>
     <div class="dashboardBlocks">
         <?php while($row = $postTypes->fetch_assoc()) : ?>
             <?php 
-                $postCount = $mysqli->query("SELECT COUNT(*) FROM `posts` WHERE post_type = '{$row['name']}'");
+                $postCount = $mysqli->query("SELECT COUNT(*) FROM `posts` WHERE post_type_id = '{$row['id']}'");
                 
-                if($postCount == false ? $postCount = 0 : $postCount = $postCount->fetch_array()[0]);
+                $postCount = ($postCount == false ? 0 : $postCount->fetch_array()[0]);
         
-                //$posts = $mysqli->query("SELECT name, author, last_edited FROM `posts` WHERE post_type = '{$row['name']}' ORDER by last_edited LIMIT 5"); 
+                $posts = $mysqli->query("SELECT name, author, last_edited FROM `posts` WHERE post_type_id = '{$row['id']}' ORDER by last_edited DESC LIMIT 5");
             ?>
         
             <div class="dashboardBlock" id="post-type">
                 <div class="counter">
-                    <h2><span class="count">
-                        <?php echo $postCount; ?></span> <?php echo ucwords(str_replace('-', ' ', $row['name'])); ?></h2>
+                    <h2>
+                        <span class="count">
+                            <?php echo $postCount; ?>
+                        </span> 
+                        <?php echo ($postCount == 1 ? rtrim(ucwords(str_replace('-', ' ', $row['name'])), 's') : ucwords(str_replace('-', ' ', $row['name']))); ?>
+                    </h2>
                 </div>
                 
-                <?php if($postCount > 0)  :?>
-                <div class="postList">
-                    <h3>Latest Edits:</h3>
-                    <ul>
-                        <li><strong>Page Name</strong><br> 01/01/2019 09:00 by Admin</li>
-                        <li><strong>Page Name</strong><br> 01/01/2019 09:00 by Admin</li>
-                        <li><strong>Page Name</strong><br> 01/01/2019 09:00 by Admin</li>
-                        <li><strong>Page Name</strong><br> 01/01/2019 09:00 by Admin</li>
-                        <li><strong>Page Name</strong><br> 01/01/2019 09:00 by Admin</li>
-                    </ul>
-                </div>
+                <?php if($postCount > 0 && $posts->num_rows > 0)  :?>
+                    <div class="postList">                        
+                        <h3>Latest Edits:</h3>
+                        <ul>
+                            <?php while($row = $posts->fetch_assoc()) : ?>
+                                <?php $row['author'] = ($row['author'] == null || $row['author'] == '' ? 'Unknown' : $row['author']); ?>
+                                <li><strong><?php echo $row['name'];?></strong><br> <?php echo date('d/m/Y H:i', strtotime($row['last_edited'])) . ' by ' . $row['author']; ?></li>
+                            <?php endwhile; ?>
+                        </ul>
+                    </div>
                 <?php endif; ?>
             </div>
         <?php endwhile; ?>
