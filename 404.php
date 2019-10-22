ff98fba74841d692bb2028c76c4f396c72394831
Edward Hall <edward.hall.144@gmail.com>
2019-10-22 09:04:18 +0100

    Created 404 page

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php http_response_code(404); ?>
+
+<?php require_once('includes/header.php'); ?>
+
+<div class="content">
+    <h1>404 - The page you have requested does not exist.</h1>
+    
+    <a href="<?php echo ROOT_DIR; ?>">Return home</a> or 
+    <a href="javascript:history.back()">Go back</a>
+</div>
+
+<?php require_once('includes/footer.php'); ?>
