b2b7e1aa547bc79563aa97790a9bd7c9f1dba26f
Ed Hall <edward.hall.144@gmail.com>
2020-03-09 09:51:02 +0000

    Added meta viewport tag

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,7 @@
 <html>
     <head>
         <?php echo metaData(); ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <link href="<?php echo ROOT_DIR ?>includes/style.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Roboto:300&display=swap" rel="stylesheet">
