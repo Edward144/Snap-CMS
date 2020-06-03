7921938bbe34465f263496876f94bc0afeebe13e
Edward144 <edward.hall.144@gmail.com>
2020-06-03 09:59:25 +0100

    Fixed missing parameter from prepared statement

--- admin/scripts/editPostType.php
+++ admin/scripts/editPostType.php
@@ -3,7 +3,7 @@
     require_once('../../includes/database.php');
 
     $details = $mysqli->prepare("UPDATE `post_types` SET title = ?, content = ?, image_url = ?, meta_title = ?, meta_description = ?, meta_keywords = ?, meta_author = ? WHERE id = ?");
-    $details->bind_param('ssssssi', $_POST['title'], $_POST['content'], $_POST['imageUrl'], $_POST['metaTitle'], $_POST['metaDescription'], $_POST['metaKeywords'], $_POST['metaAuthor'], $_POST['typeId']);
+    $details->bind_param('sssssssi', $_POST['title'], $_POST['content'], $_POST['imageUrl'], $_POST['metaTitle'], $_POST['metaDescription'], $_POST['metaKeywords'], $_POST['metaAuthor'], $_POST['typeId']);
     $ex = $details->execute();
 
     if($ex === false) {
