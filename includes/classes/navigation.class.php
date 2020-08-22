0b68b98c631f5a9bd8949a36ff25a88c09841868
Ed Hall <edward.hall.144@gmail.com>
2020-08-22 12:37:44 +0100

    Overhauled navigation class
    Used for horizontal navbars. Need to create vertical class too

--- includes/classes/navigation.class.php
+++ includes/classes/navigation.class.php
@@ -1,6 +1,6 @@
 <?php
 
-    class navigation {
+    class navbar {
         public $menuId;
         public $parentId;
         public $hasToggle = true;
@@ -23,69 +23,65 @@
             }
         }
         
-        public function display() {                
-            if($this->hasToggle == true) {
-                $this->output = '<div class="navToggle" id="hidden"></div>';
-            }
-            
-            $this->output .=
-                '<nav class="navigation" id="menu' . $this->menuId . '">';
-            
-            $this->output .=
-                $this->createLevel($this->menuId, $this->parentId);
-            
-            $this->output .=
-                '</nav>';
-            
-            echo $this->output;
+        public function display() {
+			$mysqli = $GLOBALS['mysqli'];
+			$checkMenu = $mysqli->query("SELECT COUNT(*) FROM `navigation_structure` WHERE menu_id = {$this->menuId}")->fetch_array()[0];
+			
+			if($checkMenu <= 0) {
+				return ;
+			}
+			else {
+				$this->output =
+					'<nav class="navbar navbar-expand-lg navbar-dark" id="menu' . $this->menuId . '">';
+
+				if($this->hasToggle == true) {
+					$this->output .= 
+						'<a class="navbar-brand d-lg-none d-block">Navigation</a>
+						<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar' . $this->menuId . '" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>';
+				}
+
+				$this->output .=
+					'<div class="collapse navbar-collapse" id="navbar' . $this->menuId . '">';
+
+				$this->output .=
+					$this->createLevel($this->menuId, $this->parentId);
+
+				$this->output .=
+						'</div>
+					</nav>';
+
+				echo $this->output;
+			}
         }
         
         private function createLevel($menuId = 0, $parentId = 0) {
             $mysqli = $GLOBALS['mysqli'];
-            
-            $items = $mysqli->query("SELECT * FROM `navigation_structure` WHERE menu_id = {$menuId} AND parent_id = {$parentId} ORDER BY position");
-            $itemUrl = explode('?page=', $_SERVER['REQUEST_URI'])[0];
-            $itemUrl = explode('?category=', $itemUrl)[0];
-            $count = 1;
 			
-            if($items->num_rows > 0) :
-                $output .= 
-                    '<ul class="flex-column nav border-top border-dark' . ($parentId == 0 ? '  border-bottom' : '') . '">';
-                
+            $items = $mysqli->query("SELECT * FROM `navigation_structure` WHERE menu_id = {$menuId} AND parent_id = {$parentId} ORDER BY position");
+			
+            if($items->num_rows > 0) :   
+				$output .=
+					'<ul class="navbar-nav mr-auto bg-dark">';
+			
                 while($item = $items->fetch_assoc()) :             
                     $checkChildren = $mysqli->query("SELECT id FROM `navigation_structure` WHERE menu_id = {$menuId} AND parent_id = {$item['id']}")->num_rows;
-            
-                    $output .=
-                        '<li class="nav-item' . $item['id'] . ' ' . ($count < $items->num_rows ? 'border-bottom border-dark' : '') . ' ' . ($checkChildren > 0 ? 'hasChildren' : '') . ' ' . ($this->hasImages == true ? 'hasImages' : '') . '">
-							<a class="nav-link btn btn-light rounded-0 ' . ($checkChildren > 0 ? 'd-inline-block' : '') . '" href="' . (strpos($item['url'], 'http') === false && $item['url'] != '#' ? ROOT_DIR . $item['url'] : $item['url']) . '" id="' . ($itemUrl == ROOT_DIR . $item['url'] ? 'active' : '') . '"><span>' . $item['name'] . '</span>' . ($checkChildren > 0 ? '' : '<span class="fa fa-link" style="margin-left: 18px"></span>') . '</a>' . 
-							($checkChildren > 0 ? '<span class="btn btn-light rounded-0 d-inline-block" id="dropdown"><span class="h-100 d-flex align-items-center justify-content-center"><span class="fas fa-caret-left "></span></span></span>' : '');
-                
-                        if($checkChildren > 0) : 
-                            $output .=
-                                '<div class="itemInner">
-                                    <div>';
-                            
-                            if($this->hasImages == true) : 
-                                $output .=
-                                    '<div class="navImage">' .
-                                        ($item['image_url'] != null ? '<img src="' . $item['image_url'] .'" alt="' . $item['name'] .'">' : '') 
-                                    . '</div>';
-                            endif;
-            
-                            $output .=
-                                        $this->createLevel($this->menuId, $item['id']) .
-                                    '</div>
-                                </div>';
-                        endif; 
-                    
-                    $output .=    
-                        '</li>';
 			
-					$count++;
+					$output .=
+						'<li class="nav-item' . ($checkChildren > 0 ? ' hasChildren' : '') . '">
+							<a class="nav-link text-light" href="' . $item['url'] . '">' . $item['name'] . ($checkChildren > 0 ? '<span class="ml-2 d-none d-lg-inline-block fa fa-caret-left"></span>' : '') . '</a>';
+
+					if($checkChildren > 0) :
+						$output .= $this->createLevel($this->menuId, $item['id']);
+					endif;
+					
+					$output .=
+						'</li>';
                 endwhile;
-            
-                $output .= 
-                    '</ul>';
+			
+				$this->output .=
+					'</ul>';
             endif;
             
             return $output;
