cabb9d9788502f3be04811a0bb203f9be96e8fd7
Ed Hall <edward.hall.144@gmail.com>
2020-08-22 12:37:00 +0100

    Added class for site details and checking content exits

--- includes/functions.php
+++ includes/functions.php
@@ -13,6 +13,16 @@
         }
     }
 
+	function companyDetails() {
+		global $mysqli; 
+        
+        $companyDetails = $mysqli->query("SELECT * FROM `company_info` WHERE name IS NOT NULL AND name <> '' LIMIT 1");
+		
+		if($companyDetails->num_rows == 1) {
+            return $companyDetails->fetch_assoc();
+        }
+	}
+	
 	function slugify($url) {
         return preg_replace('/[^a-zA-Z0-9\:\/\-\?\=\#\.]/', '', $url);
     }
@@ -38,6 +48,75 @@
         }
 
         return $bytes;
+    }
+
+	function checkContent($url) {   
+        global $mysqli;
+        global $_postType;
+        global $_postUrl;
+        global $postDetails;
+        
+        //Get Homepage and Check If Posts Are Hidden
+        $homepage = $mysqli->query("SELECT settings_value FROM `settings` WHERE settings_name = 'homepage'")->fetch_array()[0]; 
+        $hidePosts = $mysqli->query("SELECT settings_value FROM `settings` WHERE settings_name = 'hide posts'")->fetch_array()[0]; 
+        
+        //Get Available Post Types
+        $postTypes = $mysqli->query("SELECT name FROM `post_types`");
+        $postList = [];
+        
+        while($type = $postTypes->fetch_array()) {
+            array_push($postList, $type[0]);
+        }
+
+        if(isset($url) && strlen($url) > 0) {
+            $customUrl = explode('/', $url);
+            
+            if(in_array($customUrl[0], $postList)) {
+                $_postType = $customUrl[0];
+
+                if(isset($customUrl[1])) {
+                    unset($customUrl[0]);
+                    $_postUrl = implode('/', $customUrl);
+                }
+            }
+            else {
+                $_postType = 'pages';
+                $_postUrl = implode('/', $customUrl);
+            }
+        }
+        else {
+            $_postType = 'pages';
+            $_postUrl = $mysqli->query("SELECT url FROM `posts` WHERE id = {$homepage}")->fetch_array()[0];
+        }
+        
+        //Go to posts if no type set
+        if(!isset($_postType)) {
+            http_response_code(404);
+            include($_SERVER['DOCUMENT_ROOT'] . ROOT_DIR . '404.php');
+
+            exit();
+        }
+        
+        //404 If Trying To Access Hidden Posts
+        if(($hidePosts == 1 && $_postType == 'posts') || ($_postType == 'pages' && !isset($_postUrl))) {
+            http_response_code(404);
+            include($_SERVER['DOCUMENT_ROOT'] . ROOT_DIR . '404.php');
+
+            exit();
+        }
+
+        $postDetails = $mysqli->query("SELECT * FROM `post_types` WHERE name = '{$_postType}'");
+        
+        //Go to posts if type does not exist
+        if($postDetails->num_rows <= 0) {
+            http_response_code(404);
+            include($_SERVER['DOCUMENT_ROOT'] . ROOT_DIR . '404.php');
+
+            exit();
+        }
+        else {
+            $postDetails = $postDetails->fetch_assoc();
+        }
     }
 
 ?>
