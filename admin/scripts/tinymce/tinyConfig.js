0d6fa9fe2d141662f0503828204d62a5796512ea
Ed Hall <edward.hall.144@gmail.com>
2021-08-04 14:56:42 +0100

    Added better initial tinymce templates

--- admin/scripts/tinymce/tinyConfig.js
+++ admin/scripts/tinymce/tinyConfig.js
@@ -4,14 +4,16 @@ var customCss = 'css/style.min.css';
 function startTiny() {
     tinymce.init({
         selector:'textarea:not(.noTiny):not(.tinySlider)',
-        plugins: 'paste image imagetools table code save link moxiemanager media fullscreen lists template autoresize textcolor colorpicker',
+        plugins: 'paste image imagetools table code save link moxiemanager media fullscreen lists template textcolor colorpicker',
         menubar: 'file edit format insert table',
         toolbar: 'undo redo | styleselect | forecolor backcolor | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table tabledelete | fontsizeselect | link insert | code fullscreen | template',
         templates: [
-            {title: 'Two Column', description: 'A two column layout that will responsively update to a single column on smaller devices.', url: templateDir + 'two-column.html'},
-            {title: 'Three Column', description: 'A three column layout that will responsively update to a single column on smaller devices.', url: templateDir + 'three-column.html'},
+            {title: 'Two Column', description: 'Two responsive columns.', url: templateDir + 'two-column.html'},
+            {title: 'Three Column', description: 'Three responsive columns.', url: templateDir + 'three-column.html'},
+            {title: 'Four Column', description: 'Four responsive columns.', url: templateDir + 'four-column.html'},
         ],
         min_height: 350,
+        height: 800,
         max_height: 1500,
         relative_urls: true,
         remove_script_host: true,
